d5f22412681d86c0da1195cb91ada35eb5c350eb
garimaarora0600 <112503036+garimaarora0600@users.noreply.github.com>
2023-05-10 23:34:28 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,16 @@
+window.addEventListener('DOMContentLoaded',()=>{
+    var tablinks=document.getElementsByClassName("tab-links");
+    var tabcontents=document.getElementsByClassName("tab-contents");
+
+    function opentab(tabname){
+        for(tablink of tablinks){
+            tablink.classList.remove("active-link");
+        }
+        for(tabcontent of tabcontents){
+            tabcontent.classList.remove("active-tab");
+        }
+        // for(tablink of tablinks){
+        //     tablink.classList.remove("active-link");
+        // }
+    }
+})
